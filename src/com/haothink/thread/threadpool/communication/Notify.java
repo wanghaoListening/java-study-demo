e1fd086afcfb0eb15902bb6c9dc22979a584e65a
王浩 <haowang162@creditease.cn>
2019-05-27 16:29:07 +0800

    模拟java线程池

---
+++ src/com/haothink/thread/threadpool/communication/Notify.java
@@ -0,0 +1,14 @@
+package com.haothink.thread.threadpool.communication;
+
+/**
+ * @author wanghao
+ * @date 2019年05月27日 16:02
+ * description:
+ */
+public interface Notify {
+
+    /**
+     * 回调
+     */
+    void notifyListen() ;
+}
