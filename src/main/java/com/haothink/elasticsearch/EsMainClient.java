4e1d3227b0b28bdcd6b52bcc3607451650ec1984
王浩 <haowang162@creditease.cn>
2019-06-21 17:53:56 +0800

    添加es配置

--- src/main/java/com/haothink/elasticsearch/EsMainClient.java
+++ src/main/java/com/haothink/elasticsearch/EsMainClient.java
@@ -1,6 +1,9 @@
 package com.haothink.elasticsearch;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.nio.entity.NStringEntity;
 import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
@@ -22,14 +25,19 @@ public class EsMainClient {
 
     private static void lowLevelGetCat() throws IOException {
         RestClient restClient = RestClient.builder(
-                new HttpHost("10.143.130.196", 9200, "http")).build();
+                new HttpHost("10.143.130.666", 9200, "http")).build();
 
         Request request = new Request(
                 "GET",
-                "/_cat");
+                "/_cluster/health");
+        HttpEntity entity = new NStringEntity("", ContentType.APPLICATION_JSON);
+        request.setEntity(entity);
         Response response = restClient.performRequest(request);
 
         System.out.println(EntityUtils.toString(response.getEntity()));
+
+        restClient.close();
+
     }
 
 
