1033c48799f3cddcd887dbde6dcb3ff5b9a0f53b
hiwanghao <hiwanghao@didiglobal.com>
2019-10-23 18:16:38 +0800

    JsonInterCngXmlUtil

--- src/main/java/com/haothink/utils/JsonInterCngXmlUtil.java
+++ src/main/java/com/haothink/utils/JsonInterCngXmlUtil.java
@@ -1,5 +1,7 @@
 package com.haothink.utils;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import de.odysseus.staxon.json.JsonXMLConfig;
 import de.odysseus.staxon.json.JsonXMLConfigBuilder;
 import de.odysseus.staxon.json.JsonXMLInputFactory;
@@ -13,6 +15,8 @@ import javax.xml.stream.XMLOutputFactory;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,7 +24,7 @@ import java.util.regex.Pattern;
  * Created by wanghao on 2019-10-22
  * mail:wanghaotime@qq.com
  **/
-public class JsonInterchXmlUtil {
+public class JsonInterCngXmlUtil {
 
 
     private static final String XML_HEADER_SIGN = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
@@ -35,22 +39,20 @@ public class JsonInterchXmlUtil {
                 "<Data>"+
                 "<Row>"+
                 "<PERSONID>35020500200610000000000701355116</PERSONID>"+
-                "<XM>吴聪楠</XM><SFZH>350624198908052530</SFZH>"+
+                "<XM>王浩1</XM><SFZH>350624198908052530</SFZH>"+
                 "</Row>"+
                 "<Row>"+
                 "<PERSONID>35020500200610000000000701355117</PERSONID>"+
-                "<XM>吴聪楠2</XM><SFZH>350624198908052531</SFZH>"+
+                "<XM>王浩2</XM><SFZH>350624198908052531</SFZH>"+
                 "</Row>"+
                 "</Data>"+
                 "</ROOT>";
 
 
-        String jsonStr = xml2json(xml);
-        System.out.println(jsonStr);
+        String json ="{\"a\":{\"b\":\"bvalue\",\"c\":\"cvalue\",\"d\":{\"dm\":\"dmvalue\"}}}\n";
 
-        System.out.println(json2xml(jsonStr));
-        System.out.println(json2xmlRemoveHeaderSign(jsonStr));
-        System.out.println(json2xmlReplaceBlank(jsonStr));
+        System.out.println(json2Map(json));
+        System.out.println(xml2Map(xml));
     }
 
 
@@ -86,6 +88,72 @@ public class JsonInterchXmlUtil {
         return xmlStr;
     }
 
+    public static Map json2Map(String json){
+
+        JSONObject rootObject = JSONObject.parseObject(json);
+        Map<String,Object> rootMap = new LinkedHashMap<>(rootObject.size());
+        json2Map(rootObject,rootMap);
+        return rootMap;
+    }
+
+    /**
+     *
+     * 递归遍历jsonObject下的元素
+     *
+     */
+    private static void json2Map(JSONObject rootObject,Map<String,Object> rootMap){
+
+        for(Map.Entry<String, Object> entry : rootObject.entrySet()){
+
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            if(value instanceof JSONObject){
+                JSONObject childObject = (JSONObject)value;
+                Map<String,Object> childMap = new LinkedHashMap<>(childObject.size());
+                json2Map(childObject,childMap);
+                rootMap.put(key,childMap);
+            }else if(value instanceof String){
+                rootMap.put(key,value);
+            }else if(value instanceof JSONArray){
+                //value是jsonArray的情况暂时忽略
+            }else{
+                rootMap.put(key,value);
+            }
+
+        }
+    }
+
+
+    private static JSONObject json2Object(String json){
+
+        JSONObject object = new JSONObject();
+        try {
+            JSONObject jsonObject = JSONObject.parseObject(json);
+            for(Map.Entry<String, Object> entry : jsonObject.entrySet()){
+                String key = entry.getKey();
+                Object value = entry.getValue();
+                if(value instanceof JSONObject){
+                    JSONObject childObject = (JSONObject)value;
+                    Map<String,Object> childMap = new LinkedHashMap<>(childObject.size());
+                    json2Map(childObject,childMap);
+                    object.put(key,childMap);
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return object;
+
+    }
+
+
+    public static Map xml2Map(String xml){
+
+       String jsonStr = xml2json(xml);
+        System.out.println(jsonStr);
+       return json2Map(jsonStr);
+    }
+
     public static String xml2json(String xml) {
         StringReader input = new StringReader(xml);
         StringWriter output = new StringWriter();
@@ -108,6 +176,8 @@ public class JsonInterchXmlUtil {
         }
         return output.toString();
     }
+
+
 
 
     public static String json2xmlReplaceBlank(String json) {
