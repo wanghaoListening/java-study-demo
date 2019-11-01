caffcf23682d7963c5cde6343726572e8a023dae
hiwanghao <hiwanghao@didiglobal.com>
2019-11-01 19:15:29 +0800

    XML转map工具

---
+++ src/main/java/com/haothink/dom4j/XmlParserDemo.java
@@ -0,0 +1,114 @@
+package com.haothink.dom4j;
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.io.IOUtils;
+import org.dom4j.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+/**
+ * Created by wanghao on 2019-11-01
+ * mail:wanghaotime@qq.com
+ **/
+public class XmlParserDemo {
+
+
+    public static void main(String[] args) throws IOException, DocumentException {
+
+        InputStream inputStream = XmlParserDemo.class.getClassLoader().getResourceAsStream("response_demo.xml");
+
+        StringWriter writer = new StringWriter();
+        IOUtils.copy(inputStream, writer, StandardCharsets.UTF_8.name());
+        String xml = writer.toString();
+        Map<String, Object> map = xml2Map(xml);
+        System.out.println(JSONObject.toJSONString(map));
+        System.out.println(map2Xml(map,"Application"));
+    }
+
+
+    public static String map2Xml(Map<String, Object> map,String rootElement){
+
+        Document doc = DocumentHelper.createDocument();
+        Element body = DocumentHelper.createElement(rootElement);
+        doc.add(body);
+        buildMap2xmlBody(body, map);
+        doc.setXMLEncoding("UTF-8");
+        return doc.asXML();
+    }
+
+
+
+    public static Map<String, Object> xml2Map(String xml) throws DocumentException {
+        if (xml == null || "".equals(xml)) {
+            return null;
+        }
+        Document doc = DocumentHelper.parseText(xml);
+        Element root = doc.getRootElement();
+        return Dom2Map(root);
+    }
+
+
+
+
+    private static void buildMap2xmlBody(Element body,Map<String, Object> map){
+        if(null == map || map.isEmpty()){
+            return;
+        }
+
+        for(Map.Entry<String, Object> entry : map.entrySet()){
+            Object value = entry.getValue();
+            String key = entry.getKey();
+            Element element = DocumentHelper.createElement(key);
+            if(value instanceof Map){
+                Map<String,Object> valueMap = (Map<String,Object>)value;
+                buildMap2xmlBody(element,valueMap);
+                body.add(element);
+            }else{
+                Attribute attribute = DocumentHelper.createAttribute(body,key,value.toString());
+                body.add(attribute);
+            }
+        }
+    }
+
+
+    private static Map<String, Object> Dom2Map(Element root) {
+
+        Map<String, Object> map = new HashMap<>();
+
+        for (Iterator iterator = root.elementIterator(); iterator.hasNext(); ) {
+            Map<String, Object> eleMap = new HashMap<>();
+            Element e = (Element) iterator.next();
+            List attributeList = e.attributes();
+            for (Object o : attributeList) {
+                if (o instanceof Attribute) {
+                    Attribute attribute = (Attribute) o;
+                    eleMap.put(attribute.getName(), attribute.getValue());
+                }
+            }
+            List elements = e.elements();
+            if (elements.size() > 0) {
+                //将子元素继续执行
+                for(Object o : elements) {
+                    Element childEle = (Element)o;
+                    eleMap.put(childEle.getName(), Dom2Map(childEle));
+                }
+            }
+            map.put(e.getName(), eleMap);
+        }
+        //将当前标签的属性装载到子标签同级别的map当中
+        List attributeList = root.attributes();
+        for (Object o : attributeList) {
+            if (o instanceof Attribute) {
+                Attribute attribute = (Attribute) o;
+                map.put(attribute.getName(), attribute.getValue());
+            }
+        }
+
+        return map;
+    }
+
+}
