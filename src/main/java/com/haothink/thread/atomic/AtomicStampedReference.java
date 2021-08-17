4ace281147eaa7fe65fcaafe9b00f09b6feda4d3
wanghaoListening <wanghaotime@gmail.com>
2021-08-17 11:20:46 +0800

    Atomic Stamped Reference

---
+++ src/main/java/com/haothink/thread/atomic/AtomicStampedReference.java
@@ -0,0 +1,13 @@
+package com.haothink.thread.atomic;
+
+/**
+ * Created by wanghao on 2021/8/17
+ **/
+public class AtomicStampedReference {
+
+  public static void main(String[] args) {
+
+  }
+
+
+}
